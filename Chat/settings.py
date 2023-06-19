e37bbd42cc4af509897a39ec08e2376d9a0545dd
Abdi <inventorabdi@gmail.com>
2023-06-19 06:30:34 -0700

    custom user model created

--- Chat/settings.py
+++ Chat/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'chatapp.CustomUser'
 
 
 # Internationalization
